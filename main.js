219553b34464f9850c333b1bd13627c3395f2b5d
Sur Saha <Surayani02@users.noreply.github.com>
2020-10-01 11:32:26 +0530

    Navigation bar added

---
+++ main.js
@@ -0,0 +1,22 @@
+// Vanilla JS Code
+
+const selectElement = (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click', () => {
+  selectElement('header').classList.toggle('active');
+});
+
+// jQuery Code
+
+$(document).ready(function () {
+  $('#search-icon').click(function () {
+    $('.hide-item').toggleClass('nav-link__hide')
+    // $('.search-form').addClass('active')
+    // $('.close').addClass('active')
+  })
+  // $('#close').click(function () {
+  //   $('.hide-item').remove('nav-link__hide')
+  //   $('.search-form').remove('active')
+  //   $('.close').remove('active')
+  // })
+})
